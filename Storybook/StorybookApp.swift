9549439a9de24d293a02eafc26e7a09a0bbbafdb
bo <q463250938@gmail.com>
2024-02-15 17:21:45 -0800

    Initial Commit

---
+++ Storybook/StorybookApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorybookApp.swift
+//  Storybook
+//
+//  Created by Bo Zhong on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct StorybookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
